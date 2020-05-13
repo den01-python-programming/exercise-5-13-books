2bb61bf2946a342baae89e3c4a25e0c040d524c3
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-13 13:12:16 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,9 +1,12 @@
 import pytest
-import src.exercise
+import os
 
 def test_exercise():
+
+    os.chdir('src')
+    import exercise
+
     input_values = ["Nothing","2010","Random","2013","Random","2013",""]
-    input_values_stored = ["1","2"]
     output = []
 
     def mock_input(s=None):
@@ -14,12 +17,12 @@ def test_exercise():
             output.append("")
             return input_values.pop(0)
 
-    src.exercise.input = mock_input
-    src.exercise.print = lambda s : output.append(s)
+    exercise.input = mock_input
+    exercise.print = lambda s : output.append(s)
 
-    src.exercise.main()
+    exercise.main()
 
     assert output == ["Name (empty will stop):","Publication year:",\
                         "Name (empty will stop):","Publication year:",\
-                            "Name (empty will stop):","Publication year:","The book is already on the list. Let's not add the same book again."\
+                            "Name (empty will stop):","Publication year:","The book is already on the list. Let's not add the same book again.",\
                                 "Name (empty will stop):","Thank you! Books added: 2"]
